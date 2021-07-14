6a67f57a0e4569a616885e4dc52fc06359daa4bb
indhuJothi <indhu.palanivel@aspiresys.com>
2021-07-14 13:22:37 +0530

    First-commit

---
+++ post/src/app/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../index.css'
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <section>
+        <h1>Redux Essentials Example</h1>
+
+        <div className="navContent">
+          <div className="navLinks"></div>
+        </div>
+      </section>
+    </nav>
+  )
+}
